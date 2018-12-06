d78cf127bfcc8acde1d24cf76617b42063ee412a
Raha Pirzadeh <pirzadeh@chapman.edu>
2018-12-06 10:04:48 -0800

    Add files via upload

--- test_sombrero.py
+++ test_sombrero.py
@@ -1,7 +1,19 @@
 ###
-# Name:Gabriella Nutt
+# Name:Gabriella Nutt 
 #Student ID: 2307512
 #Email: nutt@chapman.edu
+# Raha Pirzadeh 
+#Student ID: 2290732
+#Email: pirzadeh@chapman.edu
 #Course: PHYS220/MATH220/CPSC220 Fall 2018
 #Assignment: CW12
 ###
+
+import numpy as np
+import numba as nb
+import matplotlib.pyplot as plt
+import sombrero as som
+
+def test_sombrero():
+    assert som.sombrero(-0.9, 0, np.arange(0, 2*np.pi*50, 0.001), 0.18)[-1] < -0.816
+    assert som.sombrero(-0.9, 0, np.arange(0, 2*np.pi*50, 0.001), 0.18)[-1] > -0.816
